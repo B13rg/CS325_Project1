72fc76e5549d5511e31332f0f0e060f4de39bccb
B13rg <b11484@gmail.com>
2017-04-21 01:44:25 -0700

    Setup file input and output

--- alg1.cpp
+++ alg1.cpp
@@ -1,1 +1,15 @@
 //Enumeration
+#include <iostream>
+#include <vector>
+#include "file_io.h"
+
+using namespace std;
+
+int main(){
+
+	vector< vector<int> > data = file_input();
+		
+	
+	file_output(data[0], data[0]);
+	return 0;
+}
