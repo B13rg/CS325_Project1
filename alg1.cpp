2a1e331c6d65f7bbcf4a777ccfd1697e30fe5a7b
B13rg <b11484@gmail.com>
2017-04-22 14:15:04 -0700

    Alg1 work in progress

--- alg1.cpp
+++ alg1.cpp
@@ -5,11 +5,44 @@
 
 using namespace std;
 
+vector<int> enumeration(vector<int> data){
+	int sum=0, temp=0;
+	int index[2];
+	vector<vector<int> > holder;
+	for(int i=0; i<data.size()-1; i++){
+		vector<int> blue(data.size());
+		for(int j=i; j<data.size(); j++){
+			temp += data[j];
+			blue[j]=temp;
+		}
+		holder.push_back(blue);
+	}
+
+	//Search for largest
+	for(int i=0; i<data.size()-1; i++){
+		for(int j=i; j<data.size(); j++){
+			if(holder[i][j] > sum){
+				index[0] = i;
+				index[1] = j;
+			}
+		}
+	}
+
+	vector<int>::const_iterator first = data.begin()+index[0];
+	vector<int>::const_iterator last = data.begin()+index[1]+1;
+	vector<int> newvec (first, last);
+	return newvec;
+}
+
 int main(){
 
 	vector< vector<int> > data = file_input();
-		
+	vector<int> temp = enumeration(data[0]);
+
+	for(int i=0; i<temp.size(); i++)
+		cout << temp[i] << " ";
 	
-	file_output(data[0], data[0]);
+
+	//file_output(data[0], data[0]);
 	return 0;
 }
